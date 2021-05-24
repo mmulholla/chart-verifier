41d65f8d442c81cf59d4e94acd352cfa68ad97aa
Martin Mulholland <mmulholl@redhat.com>
2021-05-24 17:55:32 -0400

    Move the check for ocp version to be part of the chart-testing check.

--- pkg/chartverifier/checks/charttesting_test.go
+++ pkg/chartverifier/checks/charttesting_test.go
@@ -1,6 +1,7 @@
 package checks
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/spf13/viper"
@@ -71,4 +72,87 @@ func TestChartTesting(t *testing.T) {
 			require.True(t, r.Ok)
 		})
 	}
+}
+
+type ocVersionError struct{}
+
+func (v ocVersionError) getVersion() (string, error) {
+	return "", errors.New("error")
+}
+
+type ocVersionWithoutError struct {
+	Version string
+}
+
+func (v ocVersionWithoutError) getVersion() (string, error) {
+	return v.Version, nil
+}
+
+type testAnnotationHolder struct {
+	OpenShiftVersion              string
+	CertifiedOpenShiftVersionFlag string
+}
+
+func (holder *testAnnotationHolder) SetCertifiedOpenShiftVersion(version string) {
+	holder.OpenShiftVersion = version
+}
+
+func (holder *testAnnotationHolder) GetCertifiedOpenShiftVersionFlag() string {
+	return holder.CertifiedOpenShiftVersionFlag
+}
+
+func TestVersionSetting(t *testing.T) {
+	type testCase struct {
+		description string
+		holder      *testAnnotationHolder
+		versioner   Versioner
+		version     string
+		error       string
+	}
+
+	testCases := []testCase{
+		{
+			description: "oc.Version returns 4.7.9",
+			holder:      &testAnnotationHolder{},
+			versioner:   ocVersionWithoutError{Version: "4.7.9"},
+			version:     "4.7.9",
+		},
+		{
+			description: "oc.Version returns error, flag set to 4.7.8",
+			holder:      &testAnnotationHolder{CertifiedOpenShiftVersionFlag: "4.7.8"},
+			versioner:   ocVersionError{},
+			version:     "4.7.8",
+		},
+		{
+			description: "oc.Version returns semantic error, flag set to fourseveneight",
+			holder:      &testAnnotationHolder{CertifiedOpenShiftVersionFlag: "fourseveneight"},
+			versioner:   ocVersionError{},
+			error:       "OpenShift version is not following SemVer spec. Invalid Semantic Version",
+		},
+		{
+			description: "oc.Version returns error, flag not set",
+			holder:      &testAnnotationHolder{},
+			versioner:   ocVersionError{},
+			error:       "Missing OpenShift version. error. And the 'openshift-version' flag has not set.",
+		},
+	}
+
+	for _, tc := range testCases {
+
+		t.Run(tc.description, func(t *testing.T) {
+
+			err := setOCVersion(&CheckOptions{AnnotationHolder: tc.holder}, tc.versioner)
+
+			if len(tc.error) > 0 {
+				require.Error(t, err)
+				require.Equal(t, tc.error, err.Error())
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.version, tc.holder.OpenShiftVersion)
+			}
+
+		})
+
+	}
+
 }

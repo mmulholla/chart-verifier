22b3fa1e5ee8f7405de3179b268b600b13a088df
Martin Mulholland <mmulholl@redhat.com>
2021-07-30 09:12:03 -0400

    work underway

--- cmd/version.go
+++ cmd/version.go
@@ -1,3 +1,19 @@
 package cmd
 
 var Version = "1.2.1"
+
+
+type Releases struct {
+	Releases  []Release       `json:"releases"`
+}
+
+type Release struct {
+	Version string        `json:"version"`
+}
+
+func getVersion() string {
+
+     
+
+
+}

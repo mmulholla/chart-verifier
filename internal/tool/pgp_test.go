69ca4cc9f60696f5270f725d4814f3001b34ac11
Martin Mulholland <mmulholl@redhat.com>
2022-10-17 14:24:30 -0400

    test updates

--- internal/tool/pgp_test.go
+++ internal/tool/pgp_test.go
@@ -2,7 +2,6 @@ package tool
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"os/exec"
@@ -10,10 +9,10 @@ import (
 	"testing"
 )
 
-func TestPGPKeyEncoding(t *testing.T) {
+var keyfileName = "../../tests/charts/psql-service/0.1.11/psql-service-0.1.11.tgz.key"
+var expectedDigest = "1cc31121e86388fad29e4cc6fc6660f102f43d8c52ce5f7d54e134c3cb94adc2"
 
-	keyfileName := "../../tests/charts/psql-service/0.1.11/psql-service-0.1.11.tgz.key"
-	expectedDigest := "1cc31121e86388fad29e4cc6fc6660f102f43d8c52ce5f7d54e134c3cb94adc2"
+func TestPGPKeyEncoding(t *testing.T) {
 
 	encodedKey, encodeErr := GetEncodedKey(keyfileName)
 	require.NoError(t, encodeErr)
@@ -31,62 +30,19 @@ func TestPGPKeyEncoding(t *testing.T) {
 	require.NoError(t, readErr)
 	require.Equal(t, keyBytes, decodedKey)
 
-	baseCmd := fmt.Sprintf("cat %s | base64 | sha256sum", keyfileName)
-	out, baseCmdErr := exec.Command("bash", "-c", baseCmd).Output()
-	require.NoError(t, baseCmdErr)
-
-	shaResponseSplit := strings.Split(string(out), " ")
-	require.Equal(t, expectedDigest, shaResponseSplit[0])
-
-	//getShaCmd := fmt.Sprintf("%s | base64 | sha256sum", keyfileName)
-	// cmdErr := exec.Command("base64", "-i", keyfileName, "-o", "./base64key.txt").Run()
-	// require.NoError(t, cmdErr, fmt.Sprintf("Error: %v", cmdErr))
-
-	//shaResponse, shaCmdErr := exec.Command("sha256sum", "./base64key.txt").Output()
-	//removeErr := os.Remove("base64key.txt")
-	//require.NoError(t, removeErr)
-	//require.NoError(t, shaCmdErr, fmt.Sprintf("Error: %v : %s", shaCmdErr, string(shaResponse)))
-	//shaResponseSplit := strings.Split(string(shaResponse), " ")
-	//require.Equal(t, keyDigest, strings.TrimRight(shaResponseSplit[0], " -\n"))
-}
-
-func BestDigest1(t *testing.T) {
-	keyfileName := "../../tests/charts/psql-service/0.1.11/psql-service-0.1.11.tgz.key"
-	expectedDigest := "1cc31121e86388fad29e4cc6fc6660f102f43d8c52ce5f7d54e134c3cb94adc2"
-
-	base64Cmd := exec.Command("base64", "-i", keyfileName)
-	sha256cmd := exec.Command("sha256sum")
-
-	sha256Value := bytes.NewBufferString("")
-	sha256cmd.Stdin, _ = base64Cmd.StdoutPipe()
-	sha256cmd.Stdout = sha256Value
-
-	_ = sha256cmd.Start()
-	_ = base64Cmd.Run()
-	_ = sha256cmd.Wait
-
-	t.Logf("sha256: %s", sha256Value.String())
-	shaResponseSplit := strings.Split(sha256Value.String(), " ")
-	require.Equal(t, expectedDigest, strings.TrimRight(shaResponseSplit[0], " -\n"))
-
 }
 
 func TestDigest2(t *testing.T) {
-
-	keyfileName := "../../tests/charts/psql-service/0.1.11/psql-service-0.1.11.tgz.key"
-	expectedDigest := "1cc31121e86388fad29e4cc6fc6660f102f43d8c52ce5f7d54e134c3cb94adc2"
 
 	base64Cmd := exec.Command("base64", "-i", keyfileName)
 	base64KeyFromCmd, _ := base64Cmd.Output()
 
 	base64Key := strings.Trim(string(base64KeyFromCmd), " -\n")
 	base64Key = strings.Replace(base64Key, "\n", "", -1)
-	t.Logf("Base64 from cmd: -->%s<--", base64Key)
 
 	encodedKey, encodeErr := GetEncodedKey(keyfileName)
 	require.NoError(t, encodeErr)
 	require.True(t, len(encodedKey) > 0)
-	t.Logf("Base64 from go func: -->%s<--", encodedKey)
 
 	require.Equal(t, encodedKey, base64Key)
 
@@ -101,7 +57,6 @@ func TestDigest2(t *testing.T) {
 	_ = base64Echo.Run()
 	_ = sha256cmd.Wait()
 
-	t.Logf("sha256: %s", sha256Value.String())
 	shaResponseSplit := strings.Split(sha256Value.String(), " ")
 	require.Equal(t, expectedDigest, strings.TrimRight(shaResponseSplit[0], " -\n"))
 
